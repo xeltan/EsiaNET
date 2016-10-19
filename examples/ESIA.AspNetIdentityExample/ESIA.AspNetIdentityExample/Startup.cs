df7f2a441549fe35cf88f6ea622276580a4a2655
xeltan <lettapost@gmail.com>
2016-10-19 15:28:24 +0500

    Example default ASP.NET MVC application from template

---
+++ examples/ESIA.AspNetIdentityExample/ESIA.AspNetIdentityExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ESIA.AspNetIdentityExample.Startup))]
+namespace ESIA.AspNetIdentityExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
