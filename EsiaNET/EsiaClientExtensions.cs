513fbbf2dcc9949022af63c8d7296a7697dcd5f9
xeltan <lettapost@gmail.com>
2017-09-27 20:30:26 +0500

    Fix with response checking from RESTful

--- EsiaNET/EsiaClientExtensions.cs
+++ EsiaNET/EsiaClientExtensions.cs
@@ -39,7 +39,7 @@ namespace EsiaNET
             string uri = String.Format("{0}{1}", EsiaHelpers.NormalizeUri(client.Options.RestUri, client.Options.PrnsSfx), oid);
             var response = await client.GetAsync(uri, styles);
 
-            return new PersonInfo(JObject.Parse(response));
+            return response == null ? null : new PersonInfo(JObject.Parse(response));
         }
 
         /// <summary>
@@ -68,35 +68,39 @@ namespace EsiaNET
 
             string uri = String.Format("{0}{1}/{2}?embed=(elements)", EsiaHelpers.NormalizeUri(client.Options.RestUri, client.Options.PrnsSfx), oid,
                 client.Options.CttsSfx);
-            var response = await client.GetAsync(uri, styles);
-            IDictionary<string, JToken> contactsDictionary = JObject.Parse(response);
             var result = new List<ContactInfo>();
+            var response = await client.GetAsync(uri, styles);
 
-            if ( contactsDictionary != null && contactsDictionary.ContainsKey("elements") )
+            if ( response != null )
             {
-                foreach ( var contact in contactsDictionary["elements"] )
+                IDictionary<string, JToken> contactsDictionary = JObject.Parse(response);
+
+                if ( contactsDictionary != null && contactsDictionary.ContainsKey("elements") )
                 {
-                    var type = contact["type"].Value<string>();
-                    ContactType contactType;
-
-                    switch ( type )
+                    foreach ( var contact in contactsDictionary["elements"] )
                     {
-                        case "MBT":
-                            contactType = ContactType.Mobile;
-                            break;
-                        case "EML":
-                            contactType = ContactType.Email;
-                            break;
-                        case "CEM":
-                            contactType = ContactType.Cem;
-                            break;
-                        case "PHN":
-                        default:
-                            contactType = ContactType.Phone;
-                            break;
-                    }
+                        var type = contact["type"].Value<string>();
+                        ContactType contactType;
 
-                    result.Add(new ContactInfo(contactType, contact["value"].Value<string>(), contact["vrfStu"].Value<string>() == "VERIFIED"));
+                        switch ( type )
+                        {
+                            case "MBT":
+                                contactType = ContactType.Mobile;
+                                break;
+                            case "EML":
+                                contactType = ContactType.Email;
+                                break;
+                            case "CEM":
+                                contactType = ContactType.Cem;
+                                break;
+                            case "PHN":
+                            default:
+                                contactType = ContactType.Phone;
+                                break;
+                        }
+
+                        result.Add(new ContactInfo(contactType, contact["value"].Value<string>(), contact["vrfStu"].Value<string>() == "VERIFIED"));
+                    }
                 }
             }
 
@@ -129,17 +133,21 @@ namespace EsiaNET
 
             string uri = String.Format("{0}{1}/{2}?embed=(elements)", EsiaHelpers.NormalizeUri(client.Options.RestUri, client.Options.PrnsSfx), oid,
                 client.Options.AddrsSfx);
-            var response = await client.GetAsync(uri, styles);
-            IDictionary<string, JToken> addrDictionary = JObject.Parse(response);
             var result = new List<AddrInfo>();
+            var response = await client.GetAsync(uri, styles);
 
-            if ( addrDictionary != null && addrDictionary.ContainsKey("elements") )
+            if ( response != null )
             {
-                foreach ( var addr in addrDictionary["elements"] )
-                {
-                    var addrObject = addr as JObject;
+                IDictionary<string, JToken> addrDictionary = JObject.Parse(response);
 
-                    if ( addrObject != null ) result.Add(new AddrInfo(addrObject));
+                if ( addrDictionary != null && addrDictionary.ContainsKey("elements") )
+                {
+                    foreach ( var addr in addrDictionary["elements"] )
+                    {
+                        var addrObject = addr as JObject;
+
+                        if ( addrObject != null ) result.Add(new AddrInfo(addrObject));
+                    }
                 }
             }
 
@@ -172,17 +180,21 @@ namespace EsiaNET
 
             string uri = String.Format("{0}{1}/{2}?embed=(elements)", EsiaHelpers.NormalizeUri(client.Options.RestUri, client.Options.PrnsSfx), oid,
                 client.Options.DocsSfx);
-            var response = await client.GetAsync(uri, styles);
-            IDictionary<string, JToken> docDictionary = JObject.Parse(response);
             var result = new List<DocInfo>();
+            var response = await client.GetAsync(uri, styles);
 
-            if ( docDictionary != null && docDictionary.ContainsKey("elements") )
+            if ( response != null )
             {
-                foreach ( var doc in docDictionary["elements"] )
-                {
-                    var docObject = doc as JObject;
+                IDictionary<string, JToken> docDictionary = JObject.Parse(response);
 
-                    if ( docObject != null ) result.Add(new DocInfo(docObject));
+                if ( docDictionary != null && docDictionary.ContainsKey("elements") )
+                {
+                    foreach ( var doc in docDictionary["elements"] )
+                    {
+                        var docObject = doc as JObject;
+
+                        if ( docObject != null ) result.Add(new DocInfo(docObject));
+                    }
                 }
             }
 
@@ -215,17 +227,21 @@ namespace EsiaNET
 
             string uri = String.Format("{0}{1}/{2}?embed=(elements)", EsiaHelpers.NormalizeUri(client.Options.RestUri, client.Options.PrnsSfx), oid,
                 client.Options.KidsSfx);
-            var response = await client.GetAsync(uri, styles);
-            IDictionary<string, JToken> kidsDictionary = JObject.Parse(response);
             var result = new List<PersonInfo>();
+            var response = await client.GetAsync(uri, styles);
 
-            if ( kidsDictionary != null && kidsDictionary.ContainsKey("elements") )
+            if ( response != null )
             {
-                foreach ( var child in kidsDictionary["elements"] )
-                {
-                    var childObject = child as JObject;
+                IDictionary<string, JToken> kidsDictionary = JObject.Parse(response);
 
-                    if ( childObject != null ) result.Add(new PersonInfo(childObject));
+                if ( kidsDictionary != null && kidsDictionary.ContainsKey("elements") )
+                {
+                    foreach ( var child in kidsDictionary["elements"] )
+                    {
+                        var childObject = child as JObject;
+
+                        if ( childObject != null ) result.Add(new PersonInfo(childObject));
+                    }
                 }
             }
 
@@ -259,17 +275,21 @@ namespace EsiaNET
 
             string uri = String.Format("{0}{1}/{2}/{3}/{4}?embed=(elements)", EsiaHelpers.NormalizeUri(client.Options.RestUri, client.Options.PrnsSfx), oid,
                 client.Options.KidsSfx, childOid, client.Options.DocsSfx);
-            var response = await client.GetAsync(uri, styles);
-            IDictionary<string, JToken> docDictionary = JObject.Parse(response);
             var result = new List<DocInfo>();
+            var response = await client.GetAsync(uri, styles);
 
-            if ( docDictionary != null && docDictionary.ContainsKey("elements") )
+            if ( response != null )
             {
-                foreach ( var doc in docDictionary["elements"] )
-                {
-                    var docObject = doc as JObject;
+                IDictionary<string, JToken> docDictionary = JObject.Parse(response);
 
-                    if ( docObject != null ) result.Add(new DocInfo(docObject));
+                if ( docDictionary != null && docDictionary.ContainsKey("elements") )
+                {
+                    foreach ( var doc in docDictionary["elements"] )
+                    {
+                        var docObject = doc as JObject;
+
+                        if ( docObject != null ) result.Add(new DocInfo(docObject));
+                    }
                 }
             }
 
@@ -302,17 +322,21 @@ namespace EsiaNET
 
             string uri = String.Format("{0}{1}/{2}?embed=(elements)", EsiaHelpers.NormalizeUri(client.Options.RestUri, client.Options.PrnsSfx), oid,
                 client.Options.VhlsSfx);
-            var response = await client.GetAsync(uri, styles);
-            IDictionary<string, JToken> vehicleDictionary = JObject.Parse(response);
             var result = new List<VehicleInfo>();
+            var response = await client.GetAsync(uri, styles);
 
-            if ( vehicleDictionary != null && vehicleDictionary.ContainsKey("elements") )
+            if ( response != null )
             {
-                foreach ( var vehicle in vehicleDictionary["elements"] )
-                {
-                    var vehicleObject = vehicle as JObject;
+                IDictionary<string, JToken> vehicleDictionary = JObject.Parse(response);
 
-                    if ( vehicleObject != null ) result.Add(new VehicleInfo(vehicleObject));
+                if ( vehicleDictionary != null && vehicleDictionary.ContainsKey("elements") )
+                {
+                    foreach ( var vehicle in vehicleDictionary["elements"] )
+                    {
+                        var vehicleObject = vehicle as JObject;
+
+                        if ( vehicleObject != null ) result.Add(new VehicleInfo(vehicleObject));
+                    }
                 }
             }
 
